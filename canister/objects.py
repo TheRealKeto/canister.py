8f9c14153b795ca8a85eded8836c7392663132c9
TheRealKeto <therealketo@gmail.com>
2021-08-26 16:05:00 -0400

    feat(client): Make use of the package endpoint
    * Add get_packages, gets a list of packages
    
    * Typehint new additions of functions and objects
    
    * Add CanisterPackage, represents a package response
    
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- canister/objects.py
+++ canister/objects.py
@@ -5,6 +5,7 @@
 from typing import (
     Dict,
     List,
+    Union,
     NamedTuple
 )
 
@@ -22,3 +23,51 @@ class CanisterAPIResponse(NamedTuple):
     # Data response of the API request
     # Raw data, handled by specific class objects
     data: List[Dict[str, str]]
+
+class CanisterPackage(object):
+    """ Independent class representation of a package object.
+
+    This class takes data from a CanisterAPIResponse object,
+    making it easier to refer to specific fields of packages. """
+    def __init__(self, data: Dict[str, str]):
+        # Get the name of the package
+        self.__name: Union[str, None] = data.get("name")
+
+        # Get the package identifier
+        # This also serves as the package name
+        self.package: str = data.get("identifier")
+
+        # Get the description of the package
+        self.description: str = data.get("description")
+
+        # Get the version of the package
+        # Only returns the latest version available
+        self.version: str = data.get("latestVersion")
+
+        # Get information about the package repo
+        # Will be used to get the URL of the repo
+        self.__repo: Dict[str, str] = data.get("repository")
+
+    def __repr__(self) -> str:
+        """ Visual represenation of the object. """
+        return f"CanisterPackage('{self.name}', '{self.description}')"
+
+    @property
+    def name(self) -> str:
+        """ Returns the name of the package.
+
+        As a fallback, this property will return the
+        package's identifier if there's no name available. """
+        # Check if there's an available package name
+        if self.__name is not None:
+            # Return the accurate package name
+            return self.__name
+
+        # All other instances, return the package identifier
+        return self.package
+
+    @property
+    def url(self) -> str:
+        """ Returns the package's repository URL. """
+        # Return the URI from the repository response
+        return self.__repo.get("uri")
