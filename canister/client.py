18821f8c93948c1d9197d8656b64b0fc4a85dbe2
TheRealKeto <therealketo@gmail.com>
2021-09-03 20:12:49 -0400

    Add basic multi-sync support
    - Implement sync support with requests
    - Rewrite most basic function in both sync and async

--- canister/client.py
+++ canister/client.py
@@ -3,12 +3,14 @@
 
 # Imports
 import aiohttp
+import requests
 
 from typing import (
     Any,
     Dict,
     List,
-    Optional
+    Optional,
+    Union
 )
 
 from .objects import (
@@ -25,7 +27,7 @@ class Client:
     def __init__(
         self,
         *,
-        session: Optional[aiohttp.ClientSession] = None,
+        session: Optional[Union[aiohttp.ClientSession, requests.Session]] = None,
         **kwargs: Any
     ):
         # Set the base endpoint of the Canister API
@@ -40,9 +42,15 @@ class Client:
             "responseFields": kwargs.pop("resp_fields", [])
         }
 
+        # Specify whether to do it async or not
+        # This makes it easier to check what session to use
+        self.__async = session is None or isinstance(
+            session, aiohttp.ClientSession
+        )
+
         # Specify the given aiohttp session globally
         # Create one session is an aiohttp session isn't given
-        self.__session = session or aiohttp.ClientSession()
+        self.__session = session
 
         # Create a list of supported API endpoints
         self.__endpoints: Dict[str, str] = {
@@ -51,13 +59,11 @@ class Client:
             "check/repo": "repositories/check"
         }
 
-    async def __search_canister(
-        self, endpoint: str, query: str
-    ) -> CanisterAPIResponse:
-        """ Make a request to the Canister API.
+    def __prepare_request(self, endpoint: str, query: str):
+        """ Prepares any request to the Canister API.
 
-        The request is made to a specificied endpoint,
-        and results will match with the given query. """
+        This function works regardless as to whether the
+        request is being done async or not. """
         # Get the specified endpoint for the request
         # then, attach it to the API base, creating a URL
         request_url = f"{self.__base}/{self.__endpoints.get(endpoint)}"
@@ -65,15 +71,71 @@ class Client:
         # Update the API params with the given query
         (params := self.__params).update({"query": query})
 
-        # Actually make a request to the Canister API
+        # Return the created URL and parameters
+        # These are important for each request made
+        return request_url, params
+
+    async def __generate_aiohttp_session(self) -> aiohttp.ClientSession:
+        """ Generates a live aiohttp session if one isn't given. """
+        # Start a live aiohttp session
+        # then, pass the live session to the client
+        self.__session = aiohttp.ClientSession()
+        return self.__session
+
+    async def __async_search_canister(
+        self, endpoint: str, query: str
+    ) -> CanisterAPIResponse:
+        """ Async request to the Canister API with aiohttp. """
+        # Check if the request is meant to be async or not
+        # then, generate a live aiohttp session if one isn't given
+        if not self.__session and self.__async:
+            self.__session = await self.__generate_aiohttp_session()
+
+        # Get the request URL and params for the request
+        request_url, params = self.__prepare_request(endpoint, query)
+
+        # Actually make a request with the given/generated aiohttp session
         async with self.__session.get(request_url, params=params) as resp:
             # Get the response of the request
-            # TODO: Add error handling to this function
+            # TODO: Do some error handling here
             response = await resp.json()
 
-        # Return the response as a CanisterResponse object
-        # This makes it easier to access requested information
+        # Return the response as a CanisterAPIResponse object
+        # This makes it easier to acess requested information
         return CanisterAPIResponse(**response)
+
+    def __sync_search_canister(
+        self, endpoint: str, query: str
+    ) -> CanisterAPIResponse:
+        """ Sync request to the Canister API. """
+        # Get the request URL and params for the request
+        request_url, params = self.__prepare_request(endpoint, query)
+
+        # Load up the response using requests
+        # This module is so slow but whatever...
+        response = self.__session.get(request_url, params=params)
+
+        # Wrap the response in a CanisterAPIResponse object
+        return CanisterAPIResponse(**response.json())
+
+    def __search_canister(
+        self, endpoint: str, query: str
+    ) -> CanisterAPIResponse:
+        """ Make a request to the Canister API.
+
+        The function is both sync and async, meaning that it works
+        with pretty much any ancient requests module version as well
+        as with aiohttp.
+
+        The request is made to a specific endpoint, and results
+        will match with the given query. """
+        # Check if async is the key...
+        if self.__async:
+            # Use aiohttp to make the API request
+            return self.__async_search_canister(endpoint, query)
+
+        # In other instances, do a sync API request with requests
+        return self.__sync_search_canister(endpoint, query)
 
     async def get_packages(self, query: str) -> List[CanisterPackage]:
         """ Obtains a list of packages based on given query.
