8d56414c48de6b97c2b479519765d2ba159b7043
TheRealKeto <therealketo@gmail.com>
2021-09-03 20:53:01 -0400

    Add multi-sync support to all other functions
    - Add multi-sync support to get_packages
    - Add multi-sync support to check_repository
    - Create a handler that creates a list of packages with given metadata

--- canister/client.py
+++ canister/client.py
@@ -75,6 +75,28 @@ class Client:
         # These are important for each request made
         return request_url, params
 
+    def __create_packages(self, metadata: Dict[str, str]) -> List[CanisterPackage]:
+        """ Creates a list of packages.
+
+        The list of packages contain given metadata from
+        a Canister API request, made either with requests
+        or aiohttp. """
+        # Create an empty list for converted packages
+        # Packages contain metadata recieved from a request
+        package_list: List[CanisterPackage] = []
+
+        # Convert each request result into a CanisterPackage object
+        for package in metadata:
+            # Convert package metadata
+            # Makes it easier to refer to data
+            new_package = CanisterPackage(package)
+
+            # Append the converted package to the list
+            package_list.append(new_package)
+
+        # Return the list of converted packages
+        return package_list
+
     async def __generate_aiohttp_session(self) -> aiohttp.ClientSession:
         """ Generates a live aiohttp session if one isn't given. """
         # Start a live aiohttp session
@@ -104,6 +126,23 @@ class Client:
         # This makes it easier to acess requested information
         return CanisterAPIResponse(**response)
 
+    async def __async_get_packages(self, query: str) -> List[CanisterPackage]:
+        """ Async version of get_packages, with aiohttp. """
+        # Search Canister for packages that match the given query
+        resp = await self.__async_search_canister("packages", query)
+
+        # Convert each response result into a CanisterPackage object
+        return self.__create_packages(resp.data)
+
+    async def __async_check_repository(self, repo_url: str) -> CanisterRepository:
+        """ Async version of check_repository, with aiohttp. """
+        # Check if the given repo URL is an unsafe repo
+        resp = await self.__async_search_canister("check/repo", repo_url)
+
+        # Get the returned metadata of the request
+        # Wrap the response in a CanisterRepository object
+        return CanisterRepository(resp.data)
+
     def __sync_search_canister(
         self, endpoint: str, query: str
     ) -> CanisterAPIResponse:
@@ -118,6 +157,23 @@ class Client:
         # Wrap the response in a CanisterAPIResponse object
         return CanisterAPIResponse(**response.json())
 
+    def __sync_get_packages(self, query: str) -> List[CanisterPackage]:
+        """ Sync version of get_packages. """
+        # Search Canister for packages that match the given query
+        resp = self.__sync_search_canister("packages", query)
+
+        # Convert each response result into a CanisterPackage object
+        return self.__create_packages(resp.data)
+
+    def __sync_check_repository(self, repo_url: str) -> CanisterRepository:
+        """ Sync version of check repository. """
+        # Check if the given URL is an unsafe repo
+        resp = self.__sync_search_canister("check/repo", repo_url)
+
+        # Get the response of the request
+        # Wrap the returned metadata in a CanisterRepository object
+        return CanisterRepository(resp.data)
+
     def __search_canister(
         self, endpoint: str, query: str
     ) -> CanisterAPIResponse:
@@ -129,51 +185,40 @@ class Client:
 
         The request is made to a specific endpoint, and results
         will match with the given query. """
-        # Check if async is the key...
+        # Use aiohttp to interact with the Canister API first
         if self.__async:
-            # Use aiohttp to make the API request
+            # Make the request and return its contents asynchronously
             return self.__async_search_canister(endpoint, query)
 
-        # In other instances, do a sync API request with requests
+        # In other instances, use requests to interact with the API
         return self.__sync_search_canister(endpoint, query)
 
-    async def get_packages(self, query: str) -> List[CanisterPackage]:
+    def get_packages(self, query: str) -> List[CanisterPackage]:
         """ Obtains a list of packages based on given query.
 
         This function uses the 'packages' endpoint in order
         to return a list of packages available in Canister
         based on the given query. """
-        # Create an empty list
-        # Used for converted package data
-        package_list: List[CanisterPackage] = []
+        # Use requests to interact with Canister first
+        # This checks if the function should be awaited
+        if not self.__async:
+            # Get the list of packages with the given query
+            return self.__sync_get_packages(query)
+        # By default, use aiohttp to get the package list
+        return self.__async_get_packages(query)
 
-        # Search Canister for packages that match the given query
-        resp = await self.__search_canister("packages", query)
-
-        # Convert each response result into a CanisterPackage object
-        for package in resp.data:
-            # Convert package data
-            # This makes it easier to refer to it
-            new_package = CanisterPackage(package)
-
-            # Append the converted data to the list
-            package_list.append(new_package)
-
-        # Return the list of converted packages
-        return package_list
-
-    async def check_repository(self, query: str) -> CanisterRepository:
+    def check_repository(self, repo_url: str) -> CanisterRepository:
         """ Checks if the given query is an unsafe repo.
 
         This is based on a list that Canister internally uses,
         which contains repositories that host malware or are
         otherwise considered as piracy. """
-        # Check if the given query is an unsafe repo
-        resp = await self.__search_canister("check/repo", query)
-
-        # Get the returned data of the request
-        # Wrap the response in a CanisterRepository object
-        return CanisterRepository(resp.data)
+        # Use requests to interact with Canister first
+        if not self.__async:
+            # Check the given repo URL if it's safe
+            return self.__sync_check_repository(repo_url)
+        # By default, use aiohttp and check the given repo URL
+        return self.__async_check_repository(repo_url)
 
     async def close(self) -> None:
         """ Closes the aiohttp session used to make requests. """
